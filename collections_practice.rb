711d2eacb548079554570082d9f7c9b7281fb698
Nabeil Lemar <nabeillemar@gmail.com>
2020-09-05 19:53:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+
+
+def sort_array_desc(array)
+  array.sort.reverse 
+end
+
+
+def sort_array_char_count(array)
+  array.sort{|x, y| x.length <=> y.length}
+end
+
+
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |x|
+    new_array << x[2] = "$"
+  end
+  end
+
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+end
+end
+
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+  unless index == 1
+    element + "s"
+  else index == 1
+    element
+  end
+  end
+end
+ 
